4b889b0c84c2347ee1e23e6cbc8e57db89b5b015
dulantzicode <115538998+dulantzicode@users.noreply.github.com>
2023-02-04 23:26:21 +0100

    Strings introduction

---
+++ strings.py
@@ -0,0 +1,11 @@
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+sentence_two = 'That is my dog\'s bowl'
+
+sentence_three = "That is my dog's bowl"
+
+sentence_four = "Tiffany said, \"That is my dog's bowl\""
+
+# la contrabarra indica que el siguiente carácer debe
+# tratarse como parte del texto y no como los caracteres 
+# de contención
