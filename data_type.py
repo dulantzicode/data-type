66617740ecf06fb6be5a6803600eaaffccd39157
dulantzicode <115538998+dulantzicode@users.noreply.github.com>
2023-02-04 22:52:23 +0100

    first commit

---
+++ data_type.py
@@ -0,0 +1,19 @@
+# boolean
+# number
+# string
+# bytes and byte arrays
+# None
+# Lists
+# Tuples
+# Sets
+# Dictionaries
+
+
+
+
+meal_completed = True
+sub_total = 100
+tip = sub_total * 1/5
+total = sub_total + tip
+receipt = "Your total is " + str(total)
+print(receipt)
